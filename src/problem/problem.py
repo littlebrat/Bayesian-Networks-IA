3bdb7e70a6cbe294e35e8529fc01ad1e2039bced
Nuno Mendes <mrgugas1993@gmail.com>
2015-12-04 02:07:08 +0000

    Stamp for main algorithm.

--- src/problem/problem.py
+++ src/problem/problem.py
@@ -5,7 +5,7 @@ class Problem:
         This class defines a query problem given the network BN, the query variable and the evidence.
     """
 
-    def __init__(self, bayesnet, query, evidence):
-        self.bn = bayesnet
-        self.query_set = query
-        self.evidence_set = evidence
+    @staticmethod
+    def ve_inference(bayesnet, query, evidence, ordering):
+
+        return 0
